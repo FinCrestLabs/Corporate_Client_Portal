b5012e1b9fc35399a0b2c131a6d03e063d6e72ee
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2024-10-21 16:36:35 +0530

    Fix: Build Dashboard for Client CMS

---
+++ frontend/constants/data.ts
@@ -0,0 +1,82 @@
+import { NavItem } from '@/types';
+
+export type User = {
+  id: number;
+  name: string;
+  company: string;
+  role: string;
+  verified: boolean;
+  status: string;
+};
+export const users: User[] = [
+  {
+    id: 1,
+    name: 'John Doe',
+    company: 'TechCorp',
+    role: 'Backend Developer',
+    verified: true,
+    status: 'Active'
+  },
+];
+
+export type Employee = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  gender: string;
+  date_of_birth: string; // Consider using a proper date type if possible
+  street: string;
+  city: string;
+  state: string;
+  country: string;
+  zipcode: string;
+  longitude?: number; // Optional field
+  latitude?: number; // Optional field
+  job: string;
+  profile_picture?: string | null; // Profile picture can be a string (URL) or null (if no picture)
+};
+
+export type Product = {
+  photo_url: string;
+  name: string;
+  description: string;
+  created_at: string;
+  price: number;
+  id: number;
+  category: string;
+  updated_at: string;
+};
+
+export const navItems: NavItem[] = [
+  {
+    title: 'Dashboard',
+    url: '/dashboard/overview',
+    icon: 'dashboard',
+    isActive: false,
+    items: [] // Empty array as there are no child items for Dashboard
+  },
+  {
+    title: 'Employee',
+    url: '/dashboard/employee',
+    icon: 'user',
+    isActive: false,
+    items: [] // No child items
+  },
+
+  {
+    title: 'Account',
+    url: '#', // Placeholder as there is no direct link for the parent
+    icon: 'billing',
+    isActive: true,
+
+    items: [
+      {
+        title: 'Profile',
+        url: '/dashboard/profile',
+        icon: 'userPen'
+      },
+    ]
+  },
+];
